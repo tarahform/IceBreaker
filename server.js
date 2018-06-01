56d232cea5604277135f8be53a404c1d9b351895
tarahform <ptarahform@gmail.com>
2018-06-01 15:09:06 -0500

    start

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+var PORT = 3000;
+var db = require("./models");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static("public"));
+require("./routes/apiRoutes")(app);
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+    db.sequelize.sync().then(function () {
+        console.log("DATABASE IS CONNECTED!");
+    });
+});
