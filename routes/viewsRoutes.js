a46251789dd5de918a4e1a21e662612723823314
SkyNet <nme312.noreply.github.com>
2018-06-04 13:22:52 -0500

    add index bios route

--- routes/viewsRoutes.js
+++ routes/viewsRoutes.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
     app.get("/", function (req, res) {
-        var bios = { anything: "anything" };
+        var bios = {};
         res.render("index", bios);
     });
 }
