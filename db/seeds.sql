388358c0df1f6c44d1e92ee2f0dacdb034028e50
Daniel A <dalamo90@gmail.com>
2018-06-05 16:52:31 -0500

    fixed seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -17,44 +17,44 @@ insert into users (first_name, middle_name, last_name, photo_link, email, phone_
 values ("Ali", "Prince", "Arfeen", "photolink", "ali.dummyemail@gmail.com", 5689930088, 18, "[6,4,3,12,9]", 5);
 
 -- challenege table data
-insert into challenge (challenge_task, point_value)
+insert into challenges (challenge_task, point_value)
 values ("Ask an employee their name.", 50);
 
-insert into challenge (challenge_task, point_value)
+insert into challenges (challenge_task, point_value)
 values ("Ask for a discount when paying for meal or drink.", 100);
 
-insert into challenge (challenge_task, point_value)
+insert into challenges (challenge_task, point_value)
 values ("Pay for the person behind you.", 75);
 
-insert into challenge (challenge_task, point_value)
+insert into challenges (challenge_task, point_value)
 values ("Ask a random person (employee or not) how their day is going.", 50);
 
-insert into challenge (challenge_task, point_value)
+insert into challenges (challenge_task, point_value)
 values ("Leave a 100% tip.", 100);
 
-insert into challenge (challenge_task, point_value)
+insert into challenges (challenge_task, point_value)
 values ("If someone is sitting alone, ask if you can share their table.", 100);
 
-insert into challenge (challenge_task, point_value)
+insert into challenges (challenge_task, point_value)
 values ("Give someone a compliment.", 50);
 
-insert into challenge (challenge_task, point_value)
+insert into challenges (challenge_task, point_value)
 values ("Ask someone what they would recommend and then order it.", 50);
 
-insert into challenge (challenge_task, point_value)
+insert into challenges (challenge_task, point_value)
 values ("Priase an employee to their manager for a job well done.", 75);
 
-insert into challenge (challenge_task, point_value)
+insert into challenges (challenge_task, point_value)
 values (" Buy a meal from wherever you are and give it away to someone in need.", 75);
 
-insert into challenge (challenge_task, point_value)
+insert into challenges (challenge_task, point_value)
 values ("Leave money in a donation jar, if the location has one.", 50);
 
-insert into challenge (challenge_task, point_value)
+insert into challenges (challenge_task, point_value)
 values ("Tell someone a joke.", 50);
 
-insert into challenge (challenge_task, point_value)
+insert into challenges (challenge_task, point_value)
 values ("Ask someone three questions and exchange LinkedIn information to network", 100);
 
-insert into challenge (challenge_task, point_value)
+insert into challenges (challenge_task, point_value)
 values ("Challenge someone to the game 'Two Trues, One Lie'.", 75);
