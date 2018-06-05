6ea35ddcc0375054452138522761d46c517d3402
tarahform <ptarahform@gmail.com>
2018-06-05 15:04:21 -0500

    jawsdb update

--- db/schema.sql
+++ db/schema.sql
@@ -5,14 +5,14 @@ use icebreakers_db;
 
 create table users(
     id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(255),
+    first_name VARCHAR(255) NOT NULL,
     middle_name VARCHAR(255),
-    last_name VARCHAR(255),
+    last_name VARCHAR(255) NOT NULL,
     photo_link VARCHAR(255),
-    email VARCHAR(255),
+    email VARCHAR(255) NOT NULL,
     phone_number INT(10),
     age INT(3),
-    member_since (DATE),
+    member_since DATE DEFAULT NOW() NOT NULL,
     challenge_id VARCHAR(255),
     user_points INT,
     primary key (id)
@@ -20,6 +20,6 @@ create table users(
 
 create table challenges(
     id INT NOT NULL AUTO_INCREMENT,
-    challenge_task VARCHAR(255),
-    point_value INT
+    challenge_task VARCHAR(255) NOT NULL,
+    point_value INT NOT NULL
 );
