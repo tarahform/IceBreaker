dbcf1fff63446976281fbb7990380cc977946a8c
tarahform <ptarahform@gmail.com>
2018-06-05 11:27:31 -0500

    map update

--- public/js/app.js
+++ public/js/app.js
@@ -5,64 +5,71 @@ AOS.init();
 // Test Map with markers //
 var map;
 var markers = [];
+// function createMarker(location) {
+//     var marker = new google.maps.Marker({
+//         map: map,
+//         position: location.latlng,
+//         title: location.name,
+//         animation: google.maps.Animation.DROP
+//     });
 
-function initMap() {
-    var chicago = { lat: 41.8781, lng: 87.6298 };
+    function initMap() {
+        var chicago = { lat: 41.8781, lng: 87.6298 };
 
-    map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
-        center: chicago,
-        mapTypeId: 'terrain'
-    });
+        map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 12,
+            center: chicago,
+            mapTypeId: 'terrain'
+        });
 
-    // This event listener will call addMarker() when the map is clicked.
-    map.addListener('click', function (event) {
-        addMarker(event.latLng);
-    });
+        // This event listener will call addMarker() when the map is clicked.
+        map.addListener('click', function (event) {
+            addMarker(event.latLng);
+        });
 
-    // Adds a marker at the center of the map.
-    addMarker(chicago);
-}
-
-// Adds a marker to the map and push to the array.
-function addMarker(location) {
-    var marker = new google.maps.Marker({
-        position: location,
-        map: map
-    });
-    markers.push(marker);
-}
-
-// Sets the map on all markers in the array.
-function setMapOnAll(map) {
-    for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(map);
+        // Adds a marker at the center of the map.
+        addMarker(chicago);
     }
-}
 
-// Removes the markers from the map, but keeps them in the array.
-function clearMarkers() {
-    setMapOnAll(null);
-}
+    // Adds a marker to the map and push to the array.
+    function addMarker(location) {
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map
+        });
+        markers.push(marker);
+    }
 
-// Shows any markers currently in the array.
-function showMarkers() {
-    setMapOnAll(map);
-}
+    // Sets the map on all markers in the array.
+    function setMapOnAll(map) {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(map);
+        }
+    }
 
-// Deletes all markers in the array by removing references to them.
-function deleteMarkers() {
-    clearMarkers();
-    markers = [];
-}
-// Test Map with markers //
+    // Removes the markers from the map, but keeps them in the array.
+    function clearMarkers() {
+        setMapOnAll(null);
+    }
 
-// FOURSQUARE API
-var clientID = "ROUJL50N3DXNBVUDYAG03BYSF4B5PYET4WWJNIQ0DGR5NOOK";
-var clientSecret = "CI1G4J1YZ4YXFCQGNVBAPVA5FGLUEKFR3VSK0ESH2BEKYANW";
-var fourSquareURL = "https://api.foursquare.com/v2/venues/search?&near=Chicago"
-    + "&client_id=" + clientID + "&client_secret=" + clientSecret
-    + "&v=20180604" + "&categoryId=4bf58dd8d48988d1e0931735";
+    // Shows any markers currently in the array.
+    function showMarkers() {
+        setMapOnAll(map);
+    }
+
+    // Deletes all markers in the array by removing references to them.
+    function deleteMarkers() {
+        clearMarkers();
+        markers = [];
+    }
+    // Test Map with markers //
+
+    // FOURSQUARE API
+    var clientID = "ROUJL50N3DXNBVUDYAG03BYSF4B5PYET4WWJNIQ0DGR5NOOK";
+    var clientSecret = "CI1G4J1YZ4YXFCQGNVBAPVA5FGLUEKFR3VSK0ESH2BEKYANW";
+    var fourSquareURL = "https://api.foursquare.com/v2/venues/search?&near=Chicago"
+        + "&client_id=" + clientID + "&client_secret=" + clientSecret
+        + "&v=20180604" + "&categoryId=4bf58dd8d48988d1e0931735";
 
     // $.ajax({
     //     method: "GET",
