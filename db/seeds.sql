e98d30a3a3ef995d4a58080ae3277882457e25be
tarahform <ptarahform@gmail.com>
2018-06-05 15:44:18 -0500

    seeds bug fix

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,16 +1,16 @@
 use icebreakers_db;
 
 insert into users (first_name, middle_name, last_name, photo_link, email, phone_number, age, challenge_id, user_points)
-values ("Nicholas", "Madrid", "Edwards", "photolink", "n.m.e.dummyemail@gmail.com", 7086622234, 18, "[1,4,6,12,10]", 5)
+values ("Nicholas", "Madrid", "Edwards", "photolink", "n.m.e.dummyemail@gmail.com", 7086622234, 18, "[1,4,6,12,10]", 5);
 
 insert into users (first_name, middle_name, last_name, photo_link, email, phone_number, age, challenge_id, user_points)
-values ("Daniel", "Remember", "Alamo", "photolink", "dalamo.dummyemail@gmail.com", 3125484991, 18, "[1,5,3,7,9]", 5)
+values ("Daniel", "Remember", "Alamo", "photolink", "dalamo.dummyemail@gmail.com", 3125484991, 18, "[1,5,3,7,9]", 5);
 
 insert into users (first_name, middle_name, last_name, photo_link, email, phone_number, age, challenge_id, user_points)
-values ("Halle", "Reid", "Conger", "photolink", "halle.dummyemail@gmail.com", 4136131284, 18, "[5,4,7,12,9]", 5)
+values ("Halle", "Reid", "Conger", "photolink", "halle.dummyemail@gmail.com", 4136131284, 18, "[5,4,7,12,9]", 5);
 
 insert into users (first_name, middle_name, last_name, photo_link, email, phone_number, age, challenge_id, user_points)
-values ("Tahrah", "Leah", "Perry", "photolink", "tahrah.dummyemail@gmail.com", 6304306422, 18, "[1,13,3,12,9]", 5)
+values ("Tahrah", "Leah", "Perry", "photolink", "tahrah.dummyemail@gmail.com", 6304306422, 18, "[1,13,3,12,9]", 5);
 
 insert into users (first_name, middle_name, last_name, photo_link, email, phone_number, age, challenge_id, user_points)
-values ("Ali", "Prince", "Arfeen", "photolink", "ali.dummyemail@gmail.com", 5689930088, 18, "[6,4,3,12,9]", 5)
+values ("Ali", "Prince", "Arfeen", "photolink", "ali.dummyemail@gmail.com", 5689930088, 18, "[6,4,3,12,9]", 5);
