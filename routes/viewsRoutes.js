56eeec2c130d9c9160eb03ea5579de76c9313ede
Halle Conger <halle.conger@gmail.com>
2018-06-06 14:47:10 -0500

    challenges from DB functioning in handlebars, no css

--- routes/viewsRoutes.js
+++ routes/viewsRoutes.js
@@ -1,3 +1,5 @@
+var db = require("../models");
+
 module.exports = function (app) {
     app.get("/", function (req, res) {
         var obj = {};
@@ -19,4 +21,12 @@ module.exports = function (app) {
         var obj = {};
         res.render("userprofile", obj);
     });
+    app.get("/challenges", function (req, res) {
+        db.Challenge.findAll({}).then(data => {
+            var obj = {
+                challenge: data
+            };
+            res.render("challenges", obj);
+        })
+    })
 }
