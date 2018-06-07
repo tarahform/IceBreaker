55021d21f48606f9521d59c459118efc0709ccd9
SkyNet <nme312.noreply.github.com>
2018-06-07 11:20:55 -0500

    unknown bug

--- public/js/app.js
+++ public/js/app.js
@@ -68,40 +68,44 @@ $(document).ready(function () {
         });
     }
 
-    // create new user //
-    function newUser() {
-        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
-            $("#signUpSubmitBtn").on("click", function () {
-                var createUserNameInput = $("#createUserNameInput").val().trim();
-                var passwordInput = $("#passwordInput").val().trim();
-            })
-        }, function (error, userData) {
-            if (error) {
-                console.log("Error creating user:", error);
-            } else {
-                console.log("Successfully created user account with uid:", userData.uid);
-            }
-        });
-        signin();
-    }
+       // create new user //
 
-    var userName, emailAddress, password;
+        // //this code creates and signs in a user everytime the app is run
+        // console.log("test user");
 
-    var ref = new Firebase("https://icebreaker-6e876.firebaseio.com/");
-    ref.createUser({
-        email: emailAddress,
-        password: password
-    }, function (error, authData) {
-        if (error) {
-            console.log("Error creating user:", error);
-        } else {
-            // save the user's profile into the database so we can list users,
-            // use them in Security and Firebase Rules, and show profiles
-            ref.child("users").child(authData.uid).set({
-                provider: authData.provider,
-                name: userName
-            });
-        }
+        // const email = "testUser2@gmail.com";
+        // const pass = "testUser2"
+        // const auth = firebase.auth();
+        // const promise = firebase.auth().createUserWithEmailAndPassword(email, pass);
+        // promise.catch(e => console.log(e.message));
+        
+    // I think the reason this does'nt work is because newUser() is never called
+    // function newUser() {
+    //     firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+    //         $("#signUpSubmitBtn").on("click", function () {
+    //             var createUserNameInput = $("#createUserNameInput").val().trim();
+    //             var passwordInput = $("#passwordInput").val().trim();
+    //         })
+    //     }, function (error, userData) {
+    //         if (error) {
+    //             console.log("Error creating user:", error);
+    //         } else {
+    //             console.log("Successfully created user account with uid:", userData.uid);
+    //         }
+    //     });
+    //     signin();
+    // }
+
+    //Sign Up On Click Function N.M.E.
+    //In theory this should create a user using the entered email and password and sign them in automatically
+    //It is basically the same code as the version that runs everytime the page refreshes, using an onClick function and JQuery
+    $("#signUpSubmitBtn").on("click", function() {
+        console.log("the function runs");
+        const email = $("#createUserNameInput").val().trim();
+        const password = $("#passwordInput").val().trim();
+        const auth = firebase.auth();
+        const promise = firebase.auth().createUserWithEmailAndPassword(email, password);
+        promise.catch(e => console.log(e.message));
     });
 
     // End of Firebase Auth //
